fea3df402217c8cbac9b5ce86dc6576215831b3d
madmdx07 <mahessdhant@gmail.com>
2023-07-17 21:11:46 +0545

    cleared the code

--- sorting_numbers.cpp
+++ sorting_numbers.cpp
@@ -2,20 +2,17 @@
 
 using namespace std;
 
-void bubbleSort(int arr[], int size){
-    int i, j, count=1;
+void sort(int arr[], int size){
+    int i, j;
 
     for(i=0; i<size; i++){
-        cout<<count<<" comparistion: "<<arr[i]<< " with";
+
         for(j=i+1; j<size; j++){
-            cout<<" "<<arr[j]<<"\n";
             if(arr[i]>arr[j]){
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
-
             }
-            count =+ 1;
            
         }
     }
@@ -31,7 +28,7 @@ void printArray(int arr[], int size)
 int main(){
     int arr[] = {1,6,9,3,2,8,7,0,4,5};
     int N = sizeof(arr)/sizeof(arr[8]);
-    bubbleSort(arr, N);
+    sort(arr, N);
     cout<<"The sorted array is:\n";
     printArray(arr, N);
     return 0;
